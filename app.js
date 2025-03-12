0c2d1aeef5994cdf6d57df3927e0a1edb8947f54
Steven <steven981214@gmail.com>
2025-03-12 16:16:13 +0800

    Remove cron.yaml and edit app.js with app.get(/)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import express from 'express';
 
 const app = express();
 const currentDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Hong_Kong' });
-const port = process.env.PORT || 8080;
+const port = process.env.PORT
 
 //Half year of current date
 const threeMonthsBefore = new Date(currentDate);
@@ -17,15 +17,19 @@ today.setDate(new Date().getDate());
 const usageAPI = `https://api.appfigures.com/v2/reports/usage?client_key=5ce2b21fe70d432b80caa05a97762d6d&group_by=date,network,products&network=firebase&start_date=${fmt(threeMonthsBefore, true)}&end_date=${fmt(today, true)}`;
 const performanceAPI = `https://api.appfigures.com/v2/reports/sales?group_by=dates,product&products=334364831261,334364831192&client_key=5ce2b21fe70d432b80caa05a97762d6d&start_date=${fmt(threeMonthsBefore, true)}&end_date=${fmt(today, true)}`;
 
-app.listen(port, async () => {
-try {
+app.get('/', async (req, res) => {
+  try {
+    res.send('The data is transferring to BigQuery, please wait for a moment.');
     await processFirebaseData(threeMonthsBefore, today, usageAPI);
-
     // After processFirebaseData finishes, call processPerformanceData
     await processPerformanceData(threeMonthsBefore, today, performanceAPI);
-
     console.log("All data processes completed successfully.");
   } catch (error) {
     console.error("An error occurred:", error);
-  }}
-);
+    res.status(500).send("An error occurred while processing data.");
+  }
+});
+
+app.listen(port, async () => {
+  console.log(`Server is running on port ${port}`);
+});
